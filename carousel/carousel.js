01dce0d43e106c16896e3c644f178b0a1c400f89
gaurav.popli <gaurav.popli@tradus.com>
2013-11-30 17:51:58 +0530

    adding carousel files

---
+++ carousel/carousel.js
@@ -0,0 +1,70 @@
+function carousel (){
+                var component = $('.carousel-viewport'),
+                    elements = component.children('a'),
+                    size = elements.size(),
+
+                    leftI = $('.left-icon'),
+                    rightI = $('.right-icon'),
+                    autoRotateInterval = 5000,
+                    currentPos = 1;
+
+                function startCarousel(){
+                    // lets start carousel by binding events and loading auto carousel
+                    bindEvents();
+
+                    autoCarousel();
+
+                }
+
+                function autoCarousel()
+                {
+                    setInterval(function(  ){
+                        rightI.trigger('click');
+                    }, autoRotateInterval );
+                }
+
+                function bindEvents(){
+                    leftI.click(function(){
+                        alert('left');
+                        loadPrevPage();
+                    });
+
+                    rightI.click(function(){
+                        loadNextPage();
+                    })
+                }
+
+                function loadPrevPage(){
+                    if( currentPos  == 1)
+                        currentPos = size;
+                    else
+                        currentPos --;
+                    startNav();
+                }
+
+                function loadNextPage(){
+                    if(currentPos == size)
+                        currentPos = 1;
+                    else
+                        currentPos ++;
+                    startNav();
+                }
+
+                function startNav(){
+                    // hide all images
+                    elements.find('div').addClass('hide');
+
+                    // show currentImage
+                    elements.find('div[id$=' + currentPos +']').removeClass('hide').addClass('show');
+
+                }
+
+                $(window).load(function(){
+                   startCarousel();
+                });
+};
+
+
+$(document).ready(function(){
+    carousel();
+});
